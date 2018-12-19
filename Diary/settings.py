bcaa6e362e863856683a4ad019bb9b8d8f83141d
gagansinghmsit <gagansinghmsit1000@gmail.com>
2018-12-19 13:52:32 +0530

    commit

--- Diary/settings.py
+++ Diary/settings.py
@@ -89,11 +89,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-    'default': dj_database_url.config(
+     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
-}
+    }
+   }
 
 
 # Password validation
