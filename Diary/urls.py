929492faad24cfef4212fe506e7042b22be70a94
gagansinghmsit <gagansinghmsit1000@gmail.com>
2018-12-21 16:49:13 +0530

    fighting

--- Diary/urls.py
+++ Diary/urls.py
@@ -3,11 +3,12 @@ from django.urls import path,include
 from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from Diary import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/saporainc./GAGAN/admin/saporainc.', admin.site.urls),
     path('',include('CreateDiary.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns +=staticfiles_urlpatterns()
 urlpatterns +=static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
 
